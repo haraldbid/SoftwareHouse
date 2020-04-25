0bb151aedbb73a94513f9f87c2f8b55d3ae5e000
Markus <54851957+Horkoth@users.noreply.github.com>
2020-04-25 17:52:00 +0200

    projectClass0003

--- src/SoftwareHouse.java
+++ src/SoftwareHouse.java
@@ -1,12 +1,17 @@
+import java.util.List;
 
 public class SoftwareHouse {
 	
-	
+	private List<Worker> listOfWorkers;
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public Worker getWorker(int index) {
+		return listOfWorkers.get(index);
 	}
 	
 	
