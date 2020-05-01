2b9d30cd585c3c4e9885412ee56cf9abb6827bc9
haraldbid <harald13245@gmail.com>
2020-05-01 13:39:09 +0200

    Timesheet test

--- src/app/TimeSheet.java
+++ src/app/TimeSheet.java
@@ -1,16 +1,27 @@
 package app;
 
+import designPatterns.Date;
+
 public class TimeSheet {
 	
-	private boolean availability;
+	//private boolean availability;
+	int minutesWorked;
+	Worker worker;
+	Worker helper;
+	Date date;
 	
 	
 	public TimeSheet(Worker worker) {
-		
-		
+		this.worker = worker;
+		this.minutesWorked = 0;
 		
 	}
-	
+	public void addtimeWorked(int hours, int minutes) {
+		this.minutesWorked += (hours*60 + minutes);
+	}
+	public void setHelper(Worker worker) {
+		this.helper = worker;
+	}
 	
 
 }
