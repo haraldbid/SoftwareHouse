265b35d26402f528862f7987d78e394213403ba8
harald Bidstrup <s183738@student.dtu.dk>
2020-05-04 14:23:02 +0200

    Date class

--- src/designPatterns/Date.java
+++ src/designPatterns/Date.java
@@ -1,5 +1,30 @@
 package designPatterns;
 
 public class Date {
+	int weekNumber, year;
+	
+	public Date(int week,int year) {
+		this.weekNumber = week;
+		this.year = year;
+	}
+
+	public int getWeekNumber() {
+		return this.weekNumber;
+	}
+
+	public void setWeekNumber(int weekNumber) {
+		if(weekNumber > 52 && weekNumber < 0) 
+			throw new IllegalArgumentException("Weeknumber must be between 0 and 52");
+		this.weekNumber = weekNumber;
+	}
+
+	public int getYear() {
+		return this.year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
 
 }
