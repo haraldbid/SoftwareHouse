f76c2e528f1a46bcaeecb2f846d8324a5cd464bc
s164055 <s164055@student.dtu.dk>
2020-05-07 16:36:48 +0200

    login feature

--- test/acceptance_test/steps/LoginSteps.java
+++ test/acceptance_test/steps/LoginSteps.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.Optional;
 
 import app.SoftwareHouse;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -19,30 +20,31 @@ public class LoginSteps {
 	
 	
 	
-	private SoftwareHouse softwareHouse;
-
-	public LoginSteps(SoftwareHouse softwareHouse) {
-		this.softwareHouse = softwareHouse;
-	}
+	private SoftwareHouse softwarehouse = new SoftwareHouse();
 	
 	
 	@Given("that no user is logged in")
 	public void thatNoUserIsLoggedIn() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    assertFalse(softwarehouse.loggedIn());
 	}
-
-	@Given("user enters ID")
-	public void userEntersID() {
+	@And("user {string} enters ID {string}") 
+	public void userEntersID(String string1, String string2){
+		softwarehouse.createWorker(string1);
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		try{
+			softwarehouse.logIn(string2);
+		}catch(Exception e) {}
 	}
-
 	@Then("user is logged in")
 	public void userIsLoggedIn() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		assertTrue(softwarehouse.loggedIn());
 	}
-
+	@Then("no user is logged in")
+	public void userIsNotLoggedIn() {
+	    // Write code here that turns the phrase above into concrete actions
+		assertFalse(softwarehouse.loggedIn());
+	}
+	
 	
 }
